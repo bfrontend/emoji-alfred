fbb3c67198c34dc84795d5b06f93c9d67c9e0f29
webxmsj <webxmsj@163.com>
2023-02-26 18:26:42 +0800

    🎉 init: 初始化emoji文件

---
+++ alx.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  outputName: 'emoji',
+  "bundleid": "com.webxmsj.emoji-alfred",
+  "category": "Tools",
+  "createdby": "webxmsj",
+  connections: {},
+  "description": "An alfred tool that use emoji for git commit message",
+  "disabled": false,
+  "name": "emoji-alfred",
+  "version": "1.0",
+  "webaddress": "https://www.webxmsj.com"
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const emojis = require('./emojis.json')
+const arg = process.argv.slice(2)
+console.log(JSON.stringify({
+  items: emojis.map(item => {
+    return {
+      title: item.title,
+      icon: {
+        type: 'image',
+        path: item.path
+      },
+      arg: `${item.icon} ${item.type}: ${arg}`
+    }
+  })
+}))
